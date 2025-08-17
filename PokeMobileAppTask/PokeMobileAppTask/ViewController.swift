03160d02e12de131e56280c12a6601427ba0d1d7
Nova Arisma <novaarisma95@gmail.com>
2025-08-17 14:00:56 +0700

    first commit

---
+++ PokeMobileAppTask/PokeMobileAppTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeMobileAppTask
+//
+//  Created by Nova Arisma on 8/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
